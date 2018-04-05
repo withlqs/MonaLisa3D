76248acc9339cf1dbdd8c3e50868bb5af087afae
zhyfzy <zhyfzy@gmail.com>
2018-04-06 00:47:41 +0800

    add stylized pictures

--- color.py
+++ color.py
@@ -5,6 +5,8 @@ from convert3d import output_3d
 
 def stylize_left(img):
     #color transform
+    shape = img.shape
+    img = cv2.resize(img, (int(img.shape[1] / img.shape[0] * 1000), 1000), interpolation=cv2.INTER_AREA)
     BGR = img.astype(np.float32) # uint8 may overflow when plus
     BGR = cv2.split(BGR)
     B = BGR[0] * 0.3
@@ -36,11 +38,28 @@ def stylize_left(img):
     HSV = HSV.astype(np.uint8)
     BGR = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
     cv2.GaussianBlur(BGR, (7,7), 0)
-    cv2.imwrite("out7.jpg",BGR)
+    cv2.imwrite("stylized_left.jpg",BGR)
+
+
+def stylize_right(img):
+    shape = img.shape
+    img = cv2.resize(img, (int(img.shape[1]/img.shape[0]*1000), 1000), interpolation=cv2.INTER_AREA)
+    BGR = img.astype(np.float32)  # uint8 may overflow when plus
+    BGR = cv2.split(BGR)
+    B = BGR[0] + 10
+    G = BGR[1] + 10
+    R = BGR[2] + 10
+    BGR = np.clip(cv2.merge([B, G, R]), 0, 255)
+    BGR = BGR.astype(np.uint8)
+    cv2.GaussianBlur(BGR, (7,7), 0)
+    cv2.imwrite("stylized_right.jpg",BGR)
+
 
 if __name__ == '__main__':
+    # print("processing ... 0%")
     imgL = cv2.imread("left.jpg")
     stylize_left(imgL)
-
-
-
+    # print("processing ... 50%")
+    imgR = cv2.imread("resized_right.jpg")
+    stylize_right(imgR)
+    # print("processing ... 100%")
